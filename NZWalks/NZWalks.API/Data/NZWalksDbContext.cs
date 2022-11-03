cf6ce26475b0bb728968f3531ba2488ab42d558f
smuzamil <sheikh.muzamil1612@gmail.com>
2022-11-03 15:54:59 +0530

    End of section 2

---
+++ NZWalks/NZWalks.API/Data/NZWalksDbContext.cs
@@ -0,0 +1,21 @@
+﻿
+
+using Microsoft.EntityFrameworkCore;
+using NZWalks.API.Models.Domain;
+
+namespace NZWalks.API.Data
+{
+    public class NZWalksDbContext : DbContext
+    {
+
+        public NZWalksDbContext(DbContextOptions<NZWalksDbContext> options) : base(options)
+        { 
+        
+        }
+
+        public DbSet<Region> Regions { get; set; } // We are tell EF to create a Region Table for us if it does not exist
+        public DbSet<Walk> Walks { get; set; }
+        public DbSet<WalkDifficulty> WalkDifficulty { get; set; }
+
+    }
+}
